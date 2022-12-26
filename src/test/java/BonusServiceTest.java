f42667df9ae95eba10d7b5915e4f13e7c3705d83
AndresKorvin <andreskorvin@gmail.com>
2022-12-26 22:00:42 +0300

    first commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -48,22 +48,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderZero() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = -1000;
-        boolean registered = true;
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-    @org.junit.jupiter.api.Test
+     @org.junit.jupiter.api.Test
     void shouldCalculateForUnregisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
@@ -100,21 +85,6 @@ public class BonusServiceTest {
 
         // подготавливаем данные:
         long amount = 0;
-        boolean registered = false;
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForUnregisteredAndUnderZero() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = -1000;
         boolean registered = false;
         long expected = 0;
 
